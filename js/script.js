b32728396c48929d5cfe04335ea4af00b1eb153b
Alexis Araza <932400662@bun2-206-01.acg.ac.nz>
2019-11-07 15:34:29 +1300

    menu

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ console.log ('La Belle Waffles!');
 $('.myicon').click(function(){
   $('nav').toggleClass("active");
 });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var ul, li, a, i;
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
